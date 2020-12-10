d3f45bac0e0e8f27d7d39afcc47be0ced3e6f78b
Mathew <mathew@admins-MacBook-Pro.local>
2020-12-10 16:14:52 -0500

    creates needed sql tables

---
+++ table_control.py
@@ -0,0 +1,32 @@
+
+from type_weight_setup import type_weight_setup
+from key_weight_setup import key_weight_setup
+from key_act_setup import key_act_setup
+from inv_act_setup import inv_act_setup
+from inv_weight_setup import inv_weight_setup
+from type_setup import type_setup
+from chord_db_drop import tab_drop
+
+def table_control(on_or_off):
+
+    def tab_setup():
+        key_weight_setup()
+        type_setup()
+        key_act_setup()
+        type_weight_setup()
+        inv_weight_setup()
+        inv_act_setup()
+    def drop():
+        tab_drop("key_act")
+        tab_drop("key_weight")
+        tab_drop("type_weight")
+        tab_drop("k_act")
+        tab_drop("inv_act")
+        tab_drop("inv_weight")
+    
+    if on_or_off == "on":
+        tab_setup()
+    elif on_or_off == "off":
+        drop()
+    
+# table_control("on")
