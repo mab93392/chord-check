ee13113cbc2f93f0e9ad371f0d6eb2fac7b75753
Mathew <mathew@admins-MacBook-Pro.local>
2020-12-14 21:50:32 -0500

    writes to accuracy table

---
+++ acc_write.py
@@ -0,0 +1,15 @@
+import mysql.connector 
+
+def acc_write(data,col_n):
+
+    # connects to database
+    chord_db = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "fm2a85xm",
+        database = "chord_data"
+    )
+
+    cur = chord_db.cursor() # establishes cursor
+
+    cur.execute("INSERT INTO accuracy( " + col_n + ") VALUES( " + data + ")" ) # inserts data
