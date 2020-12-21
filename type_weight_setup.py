9a7c344ef4f016ccc0dd6e6466df8a9f282a49e2
mathew <mbush11@vols.utk.edu>
2020-12-21 17:33:38 -0500

    altered type weight table

--- type_weight_setup.py
+++ type_weight_setup.py
@@ -17,11 +17,11 @@ def type_weight_setup():
     cur.execute("CREATE TABLE type_weight (weight_num INT AUTO_INCREMENT PRIMARY KEY, C1 FLOAT NOT NULL DEFAULT 0)")
     c_name = "C1" # begins making name statment
     for j in range(0,27):
-        k = np.random.random(22)
+        k = np.random.random(882)
         vs = "%s" % round(k[0],5) # begins making value statement
         if j == 0:
             # makes first row of table
-            for i in range(1,22):
+            for i in range(1,882):
                 c_name += ",C%s" % (i+1) # continues name statement
                 vs += ",%s" % round(k[i],5) # continue value statement
                 cur.execute("ALTER TABLE type_weight ADD (C%s FLOAT NOT NULL DEFAULT 0)" % (i+1))
@@ -29,7 +29,7 @@ def type_weight_setup():
         
         
         else: # makes other 21 rows of table
-            for i in range(1,22): 
+            for i in range(1,882): 
                 vs += ",%s" % round(k[i],5)
     
     
