701502f9f1c989d3b2af04752fc2b92a1b8a6469
mathew <mbush11@vols.utk.edu>
2020-12-21 17:54:12 -0500

    droped average accuracy column

--- acc_table_setup.py
+++ acc_table_setup.py
@@ -17,10 +17,10 @@ def acc_table_setup():
     # creates "create table statement"
     stmt1 = "CREATE TABLE accuracy " # creates table 
     stmt2 = "(test_num INT AUTO_INCREMENT PRIMARY KEY, " # makes 1st column
-    stmt3 = "av_er FLOAT NOT NULL DEFAULT 0, " # makes 2nd column
+    
     stmt4 = "total_acc FLOAT NOT NULL DEFAULT 0, " # makes 3rd column
     stmt5 = "key_acc FLOAT NOT NULL DEFAULT 0, " # makes 4th column 
     stmt6 = "inv_acc FLOAT NOT NULL DEFAULT 0, " # makes 5th column
     stmt7 = "type_acc FLOAT NOT NULL DEFAULT 0)" # makes 6th colmn
 
-    cur.execute(stmt1+stmt2+stmt3+stmt4+stmt5+stmt6+stmt7) # actually executes creation stamtent
+    cur.execute(stmt1+stmt2+stmt4+stmt5+stmt6+stmt7) # actually executes creation stamtent
