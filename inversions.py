ea3658ef1efd2737fbe3499fb769289575884dc5
mathew <mbush11@vols.utk.edu>
2021-01-14 18:06:47 -0500

    added options for no base

--- inversions.py
+++ inversions.py
@@ -1,17 +1,25 @@
 import numpy as np
 
-def inversions():
-    
-    base_opt = np.random.randint(0,11) # randomly picks inversion
-    base_pitch = []
+def inversions(key):
     A_base = 440/np.power(2,6) # generates fundemental base pitch for series of base frequencies
+    base_opt = np.random.randint(0,11) # randomly picks inversion
+
+    if key == 0:
+        A_base = 0
+    
+    base_pitch = []
+    
     inv = []
     I = []
 
     for n in range(0,4):
-        base_pitch = np.append(base_pitch,(A_base + base_opt*2*A_base/12) * np.power(2,n)) # adds base pitch
+        c_base = np.power(2,n)*A_base
+        if base_opt == 0:
+            base_pitch = np.append(base+pitch,c_base)
+        else:
+            base_pitch = np.append(base_pitch,(base_opt*2*c_base/12)) # adds base pitch
 
-        L = len(base_pitch)
+        # L = len(base_pitch)
 
         for i in range(0,1):                                        # makes mid-base range peaking 
                     xx = np.random.rand()                           # intensities with random noise
