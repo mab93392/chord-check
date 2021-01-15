6241ad341a91abca6cea2c55f8fe6debb860858c
mathew <mbush11@vols.utk.edu>
2021-01-15 15:41:10 -0500

    improved single note output

--- degrees.py
+++ degrees.py
@@ -6,21 +6,28 @@ def degrees(key,deg):
     I = []
 
     for i in range(0,deg_num):
-        for n in range(0,11):
+        if deg_num == 1:
+            oct = np.random.random_integers(3,7)
+            c_key = key*np.power(2,oct)
+            freqs = np.append(freqs,c_key) # adds first note of ocatave
+            xx = np.random.rand()
+            I = np.append(I,.333*(1/(1+np.exp(-xx))))
+        else:
+            for n in range(0,11):
             
-            k = np.power(2,n) # octave multiplier
-            c_key = key*k # first note of octave
-            if deg[i] == 0:
-                freqs = np.append(freqs,c_key) # adds first note of ocatave
-            else:
-                freqs = np.append(freqs,deg[i]*c_key*2/12) # adds subsequent notes of chord
-            
-            
-            for j in range(0,1):
-                xx = np.random.rand() 
-                t1 = abs(1 / (n-5.5)) # adds decay-from-middle-range term
-                t2 = (1 / (1+np.exp(-xx))) / 3 # adds noise term
-                I = np.append(I,(t1+t2) / 2 ) # combines terms and adds them to intensity list
+                k = np.power(2,n) # octave multiplier
+                c_key = key*k # first note of octave
+                if deg[i] == 0:
+                    freqs = np.append(freqs,c_key) # adds first note of ocatave
+                else:
+                    freqs = np.append(freqs,deg[i]*c_key*2/12) # adds subsequent notes of chord
+                
+                
+                for j in range(0,1):
+                    xx = np.random.rand() 
+                    t1 = abs(1 / (n-5.5)) # adds decay-from-middle-range term
+                    t2 = (1 / (1+np.exp(-xx))) / 3 # adds noise term
+                    I = np.append(I,(t1+t2) / 2 ) # combines terms and adds them to intensity list
 
     freqs = np.reshape(freqs,(len(freqs),1))
     I = np.reshape(I,(len(I),1))
@@ -29,4 +36,4 @@ def degrees(key,deg):
     return tone_data
 
 
-print(degrees(0,[2,4,6,10,11]))
+print(degrees(440,[0,1,4,6]))
